d443ece2e16635964a913bea94c3d94f2d86ec00
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-07-31 22:43:32 +0530

    Added Java practice programs

---
+++ Code1.java
@@ -0,0 +1,33 @@
+// Basic Operations on variables  & how to use them 
+
+public class Code1 {
+        public static void main(String [] args){
+            int a =25;
+            System.out.println(" Value of a is - "+a);
+
+            byte b = 68;
+            System.out.println("Value of b is - "+b);
+
+            short c=96;
+            System.out.println("Value of c is - "+c);
+
+            long d =4846216154l;
+            System.out.println(" value of d is - "+d);
+            
+            double e=854.3651;
+            System.out.println("Value of e is - "+e);
+
+            float f=586.352f;
+            System.out.println("Value of f is - "+f);
+
+            char g='5';
+            System.out.println("Value of g is - "+g);
+
+            boolean h=true;
+            System.out.println("Value of h is - "+h);
+
+
+        }
+
+    
+}
--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo{
     public static void main(String []arg){
  
-        System.out.println("Hello World !!!")
+        System.out.println("Hello World !!!");
 
     }
 }
