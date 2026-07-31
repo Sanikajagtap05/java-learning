0b7d594f1eee858ecefe0804b317904c50cfdd80
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-07-31 23:03:12 +0530

    Added one more practice code in the Code1

--- Code1.java
+++ Code1.java
@@ -2,29 +2,50 @@
 
 public class Code1 {
         public static void main(String [] args){
-            int a =25;
-            System.out.println(" Value of a is - "+a);
+            // int a =25;
+            // System.out.println(" Value of a is - "+a);
 
-            byte b = 68;
-            System.out.println("Value of b is - "+b);
+            // byte b = 68;
+            // System.out.println("Value of b is - "+b);
 
-            short c=96;
-            System.out.println("Value of c is - "+c);
+            // short c=96;
+            // System.out.println("Value of c is - "+c);
 
-            long d =4846216154l;
-            System.out.println(" value of d is - "+d);
+            // long d =4846216154l;
+            // System.out.println(" value of d is - "+d);
             
-            double e=854.3651;
-            System.out.println("Value of e is - "+e);
+            // double e=854.3651;
+            // System.out.println("Value of e is - "+e);
 
-            float f=586.352f;
-            System.out.println("Value of f is - "+f);
+            // float f=586.352f;
+            // System.out.println("Value of f is - "+f);
 
-            char g='5';
-            System.out.println("Value of g is - "+g);
+            // char g='5';
+            // System.out.println("Value of g is - "+g);
 
-            boolean h=true;
-            System.out.println("Value of h is - "+h);
+            // boolean h=true;
+            // System.out.println("Value of h is - "+h);
+
+            // Student Information Card
+              
+
+            String name = "Sanika";
+            System.out.println("Name : "+name );
+
+            int age = 21;
+            System.out.println("Age  : "+age);
+
+            int roll = 52;
+            System.out.println("Roll no "+roll);
+
+            String branch = "Computer Science";
+            System.out.println("Branch  : "+branch);
+
+            double cgpa = 9.14;
+            System.out.println("CGPA  : "+cgpa);
+
+            boolean hosteller = true;
+            System.out.println("Hosteller : "+hosteller);
 
 
         }
