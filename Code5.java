74f52432d257eb97763fe8190f4e75aa7d916fde
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-04 21:37:06 +0530

    Practice questions unary opretors

---
+++ Code5.java
@@ -0,0 +1,69 @@
+public class Code5 {
+    public static void main(String[] args) {
+
+        // Unary Opretors
+
+        // int a=10 ;
+        // System.out.println(a++);//First read and then increment output 10
+        // System.out.println(a);//11 here it will print incremented output
+
+        // int a=10;
+        // System.out.println(++a);
+        // // System.out.println(a);
+
+        // int b=5;
+        // // System.out.println(b++ +2);
+        // System.out.println(++b +2);
+
+        // int a = 7;
+        // System.out.println(++a + a++);
+
+        // int a = 10;
+        // System.out.println(++a + a++);//11 --> now a value 11 therefore 11+11
+
+        // System.out.println(a);//12
+
+        // int a = 10;
+
+        // System.out.println(a-- + --a);
+
+        // int a = 5;
+
+        // System.out.println(a++ + ++a + a++);
+
+        // int a = 8;
+
+        // System.out.println(--a + a-- + ++a);
+
+        // int a = 10;
+
+        // int b = a++;
+
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int a = 10;
+
+        // int b = ++a;
+
+        // System.out.println(a);
+        // System.out.println(b);
+
+        int a = 5;
+
+        int b = ++a + a++ + --a + a--;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+        int x = 3;
+
+int y = x++ + ++x + x-- + --x;
+
+System.out.println(x);
+System.out.println(y);
+
+    }
+
+}
