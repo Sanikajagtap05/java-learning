ab6c2fc6ccd21fcb5bc8f8d765bec38d5f371741
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-09 23:28:34 +0530

    if else tricky questions

--- Code7.java
+++ Code7.java
@@ -1,5 +1,6 @@
 public class Code7 {
     public static void main(String[] args) {
+        //Conditional Statements Practice Questions - IF-Else
 
         // int a = 15;
         // int b = 10;
@@ -12,9 +13,186 @@ public class Code7 {
         //     }
         // } else {
         //     System.out.println("C");
-        // }
+        // } 
+
+    //     int a=20;
+    //     if(a>=10){
+    //         if(a<=20){
+    //         System.out.println("One");
+    //     }
+    //     else
+    //     {
+    //         System.out.println("Two");
+    //     }
+    // }
+    //     else{
+    //         System.out.println("Three");
+    //     }
 
 
-        
+    // int a=10;
+    // int b=20;
+    // int c=15;
+
+    // if(a<b){
+    //     if(b>c){
+    //         if(a+c>b){
+    //             System.out.println("X");
+    //         }
+    //         else{
+    //             System.out.println("Y");
+    //         }
+    //     }
+    //     else{
+    //         System.out.println("Z");
+    //     }
+    // }
+    // else{
+    //     System.out.println("W");
+    // }
+
+
+
+    // int marks = 75;
+
+    // if (marks >=40){
+    //     if(marks >=75){
+    //         if(marks > 75){
+    //             System.out.println("Distinction");
+
+    //         }
+    //         else{
+    //             System.out.println("First Class");
+    //         }
+    //     }
+    //     else{
+    //         System.out.println("Pass");
+    //     }
+    // }
+    // else{
+    //     System.out.println("Fail");
+
+    //  int x= 5;
+    //  int y=10;
+
+    //  if(x++ < y){
+    //     if(++x == 7){
+    //         System.out.println("A");
+    //     }
+    //     else{
+    //         System.out.println("B");
+    //     }
+    //  }
+    //  else{
+    //     System.out.println("C");
+    //  }
+
+    //  System.out.println(x);
+
+
+// int a = 5;
+// int b = 10;
+
+// if (a++ > 5) {
+//     if (++b > 10) {
+//         System.out.println(a + " " + b);
+//     } else {
+//         System.out.println("X");
+//     }
+// } else {
+//     if (b++ == 10) {
+//         System.out.println(a + " " + b);
+//     } else {
+//         System.out.println("Y");
+//     }
+// }
+
+//    int n = 10;
+
+// if (n-- > 10) {
+//     if (++n == 10) {
+//         System.out.println("A");
+//     } else {
+//         System.out.println("B");
+//     }
+// } else {
+//     if (++n == 10) {
+//         System.out.println("C");
+//     } else {
+//         System.out.println("D");
+//     }
+// }
+
+// System.out.println(n);
+
+
+// int a=5;
+// int b=5;
+// int c=10;
+
+// if(a++ == b){
+//     if(++b ==c){
+//         System.out.println("X");
+//     }
+//     else{
+//         if(a+b == c){
+//             System.out.println("Y");
+//         }
+//         else{
+//             System.out.println("Z");
+//         }
+//     }
+// }
+// else{
+//     System.out.println("W");
+// }
+
+// System.out.println(a);
+// System.out.println(b);
+
+// int x = 10;
+
+// if (x++ == 10) {
+//     if (x++ == 11) {
+//         if (++x == 13) {
+//             System.out.println("A");
+//         } else {
+//             System.out.println("B");
+//         }
+//     } else {
+//         System.out.println("C");
+//     }
+// } else {
+//     System.out.println("D");
+// }
+
+// System.out.println(x);
+
+int a = 3;
+int b = 4;
+
+if (++a > 3) {
+    if (b++ > 4) {
+        System.out.println(a + b);
+    } else {
+        if (++b > 5) {
+            System.out.println(a * b);
+        } else {
+            System.out.println(a - b);
+        }
     }
+} else {
+    System.out.println(a + b);
 }
+
+
+
+
+
+
+    }
+
+    }
+
+    
+
