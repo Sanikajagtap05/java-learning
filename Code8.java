a222c94ab161ab2fb30bd51dee259536fe9bd42a
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-10 23:59:32 +0530

    Minor Changes

--- Code8.java
+++ Code8.java
@@ -1,16 +1,17 @@
 public class Code8 {
     public static void main(String[] args) {
-          //  else if 
-          int a = -15;
-          if(a > 0){
-            System.out.println("positive");
-          }
-          else if(a == 0){
-            System.out.println("Zero");
-          }
-          else if( a < 0){
-            System.out.println("Negative");
-          }
+        //  else if
+
+        //   int a = -15;
+        //   if(a > 0){
+        //     System.out.println("positive");
+        //   }
+        //   else if(a == 0){
+        //     System.out.println("Zero");
+        //   }
+        //   else if( a < 0){
+        //     System.out.println("Negative");
+        //   }
 
 
         
