72cb8114f13803d2e3150ca72b3cc80437b727f1
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-10 23:57:37 +0530

    Else if Practice

---
+++ Code8.java
@@ -0,0 +1,22 @@
+public class Code8 {
+    public static void main(String[] args) {
+          //  else if 
+          int a = -15;
+          if(a > 0){
+            System.out.println("positive");
+          }
+          else if(a == 0){
+            System.out.println("Zero");
+          }
+          else if( a < 0){
+            System.out.println("Negative");
+          }
+
+
+        
+
+
+
+    }
+    
+}
