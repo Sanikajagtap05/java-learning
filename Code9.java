c9c155ee8d802ef3b8179a586ce258640cc90a22
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-20 22:59:16 +0530

    Added code for Calculator

--- Code9.java
+++ Code9.java
@@ -1,50 +1,88 @@
 import java.util.Scanner;
 
 public class Code9 {
-    //Switch case practice questions
+    // Switch case practice questions
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int num;
+        /*
+         * int num;
+         * 
+         * System.out.print("Enter the no: ");
+         * num = sc.nextInt();
+         * 
+         * switch (num) {
+         * 
+         * case 1:
+         * System.out.println("Monday");
+         * break;
+         * 
+         * case 2:
+         * System.out.println("Tuesday");
+         * break;
+         * 
+         * case 3:
+         * System.out.println("Wednesday");
+         * break;
+         * 
+         * case 4:
+         * System.out.println("Thursday");
+         * break;
+         * 
+         * case 5:
+         * System.out.println("Friday");
+         * break;
+         * 
+         * case 6:
+         * System.out.println("Saturday");
+         * break;
+         * 
+         * case 7:
+         * System.out.println("Sunday");
+         * break;
+         * 
+         * default:
+         * System.out.println("Invalid Input");
+         * }
+         * 
+         */
 
-        System.out.print("Enter the no: ");
+        /* 
+        int a, b;
+        System.out.println("Enter the value of a - " );
+        a = sc.nextInt();
+        System.out.println("Enter the value of b - " );
+        b = sc.nextInt();
+
+        int num;
+        System.out.println("Enter your choice  : " );
         num = sc.nextInt();
 
         switch (num) {
-
             case 1:
-                System.out.println("Monday");
+                System.out.println("Addition  : " + (a + b));
                 break;
 
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("Substraction - "+(a - b));
                 break;
 
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("Multiplication " + (a * b));
                 break;
-
             case 4:
-                System.out.println("Thursday");
-                break;
-
-            case 5:
-                System.out.println("Friday");
-                break;
-
-            case 6:
-                System.out.println("Saturday");
-                break;
-
-            case 7:
-                System.out.println("Sunday");
+                System.out.println("Division :" + (a % b));
                 break;
 
             default:
-                System.out.println("Invalid Input");
+                System.out.println("Invalid Choice");
+                break;
         }
 
-    
+        */
+
+        
+
     }
 }
