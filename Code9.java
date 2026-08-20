ed718ee7138efb59c116350fb4c241ba20df461d
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-20 23:28:05 +0530

    Code for cafe menu and and traffic signal

--- Code9.java
+++ Code9.java
@@ -81,8 +81,70 @@ public class Code9 {
         }
 
         */
+   
+        /*
+        System.out.println( "------- MENU --------");
+        System.out.println("1.Pizza");
+        System.out.println("2.Burger");
+        System.out.println("3.Sandwich");
+        System.out.println("4.Pasta");
 
+        int choice ;
+        System.out.println("Enter your choice please : ");
+        choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Your Pizaa is on the way ... ");
+                System.out.println("Thank you!!!");
+                break;
+            case 2:
+                System.out.println("Your Burger is on the way ... ");
+                System.out.println("Thank you!!!");
+                break;
+               case 3:
+                System.out.println("Your Sandwich is on the way ... ");
+                System.out.println("Thank you!!!");
+                break;
+               case 4:
+                System.out.println("Your Pasta is on the way ... ");
+                System.out.println("Thank you!!!");
+                break;
         
+            default:
+                System.out.println("Invalid Choice");
+                break;
+        }
+ */
+
+
+        int num;
+        System.out.println("Enter the no - ");
+        num = sc.nextInt();
+
+        System.out.println("Traffic Signal ");
+        System.out.println("1.Red ");
+        System.out.println("2.Orange ");
+        System.out.println("3.Green ");
+
+        switch (num) {
+            case 1:
+                System.out.println("Wait until the single turns green ");
+                break;
+            case 2:
+                System.out.println("Be ready single is about to turn green  ");
+                break;
+            case 3:
+                System.out.println("Go... ");
+                break;
+            
+            default:
+                System.out.println("INVALID INPUT !!! TRY AGAIN");
+                break;
+        }
+        
+
+
 
     }
 }
