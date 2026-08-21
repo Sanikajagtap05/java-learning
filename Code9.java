2360e851246a12d74cd404c86cf5d5445da21aac
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-21 23:13:39 +0530

    Added code for vowels and for converting no to words

--- Code9.java
+++ Code9.java
@@ -48,103 +48,172 @@ public class Code9 {
          * 
          */
 
+        /*
+         * int a, b;
+         * System.out.println("Enter the value of a - " );
+         * a = sc.nextInt();
+         * System.out.println("Enter the value of b - " );
+         * b = sc.nextInt();
+         * 
+         * int num;
+         * System.out.println("Enter your choice  : " );
+         * num = sc.nextInt();
+         * 
+         * switch (num) {
+         * case 1:
+         * System.out.println("Addition  : " + (a + b));
+         * break;
+         * 
+         * case 2:
+         * System.out.println("Substraction - "+(a - b));
+         * break;
+         * 
+         * case 3:
+         * System.out.println("Multiplication " + (a * b));
+         * break;
+         * case 4:
+         * System.out.println("Division :" + (a % b));
+         * break;
+         * 
+         * default:
+         * System.out.println("Invalid Choice");
+         * break;
+         * }
+         * 
+         */
+
+        /*
+         * System.out.println( "------- MENU --------");
+         * System.out.println("1.Pizza");
+         * System.out.println("2.Burger");
+         * System.out.println("3.Sandwich");
+         * System.out.println("4.Pasta");
+         * 
+         * int choice ;
+         * System.out.println("Enter your choice please : ");
+         * choice = sc.nextInt();
+         * 
+         * switch (choice) {
+         * case 1:
+         * System.out.println("Your Pizaa is on the way ... ");
+         * System.out.println("Thank you!!!");
+         * break;
+         * case 2:
+         * System.out.println("Your Burger is on the way ... ");
+         * System.out.println("Thank you!!!");
+         * break;
+         * case 3:
+         * System.out.println("Your Sandwich is on the way ... ");
+         * System.out.println("Thank you!!!");
+         * break;
+         * case 4:
+         * System.out.println("Your Pasta is on the way ... ");
+         * System.out.println("Thank you!!!");
+         * break;
+         * 
+         * default:
+         * System.out.println("Invalid Choice");
+         * break;
+         * }
+         */
+
+        /*
+         * int num;
+         * System.out.println("Enter the no - ");
+         * num = sc.nextInt();
+         * 
+         * System.out.println("Traffic Signal ");
+         * System.out.println("1.Red ");
+         * System.out.println("2.Orange ");
+         * System.out.println("3.Green ");
+         * 
+         * switch (num) {
+         * case 1:
+         * System.out.println("Wait until the single turns green ");
+         * break;
+         * case 2:
+         * System.out.println("Be ready single is about to turn green  ");
+         * break;
+         * case 3:
+         * System.out.println("Go... ");
+         * break;
+         * 
+         * default:
+         * System.out.println("INVALID INPUT !!! TRY AGAIN");
+         * break;
+         * }
+         * 
+         */
+
+        /*
+         * String let;
+         * System.out.println("Enter the your letter  -  ");
+         * let = sc.next();
+         * 
+         * switch (let) {
+         * case "a":
+         * System.out.println("Vowel ");
+         * break;
+         * case "e":
+         * System.out.println("Vowel");
+         * break;
+         * case "i":
+         * System.out.println("Vowel");
+         * break;
+         * case "o":
+         * System.out.println("Vowel");
+         * break;
+         * case "u":
+         * System.out.println("Vowel");
+         * break;
+         * 
+         * default:
+         * System.out.println("Consonent");
+         * break;
+         * 
+         * }
+         */
+
         /* 
-        int a, b;
-        System.out.println("Enter the value of a - " );
-        a = sc.nextInt();
-        System.out.println("Enter the value of b - " );
-        b = sc.nextInt();
-
+        // no to words
         int num;
-        System.out.println("Enter your choice  : " );
+        
+        System.out.println("Enter the your no between (0-9)");
         num = sc.nextInt();
-
         switch (num) {
+            case 0:
+                System.out.println("Zero");
+                break;
             case 1:
-                System.out.println("Addition  : " + (a + b));
+                System.out.println("One");
                 break;
-
             case 2:
-                System.out.println("Substraction - "+(a - b));
+                System.out.println("Two");
                 break;
-
             case 3:
-                System.out.println("Multiplication " + (a * b));
+                System.out.println("Three");
                 break;
             case 4:
-                System.out.println("Division :" + (a % b));
+                System.out.println("Four");
                 break;
-
-            default:
-                System.out.println("Invalid Choice");
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
                 break;
         }
-
+        
         */
-   
-        /*
-        System.out.println( "------- MENU --------");
-        System.out.println("1.Pizza");
-        System.out.println("2.Burger");
-        System.out.println("3.Sandwich");
-        System.out.println("4.Pasta");
-
-        int choice ;
-        System.out.println("Enter your choice please : ");
-        choice = sc.nextInt();
-
-        switch (choice) {
-            case 1:
-                System.out.println("Your Pizaa is on the way ... ");
-                System.out.println("Thank you!!!");
-                break;
-            case 2:
-                System.out.println("Your Burger is on the way ... ");
-                System.out.println("Thank you!!!");
-                break;
-               case 3:
-                System.out.println("Your Sandwich is on the way ... ");
-                System.out.println("Thank you!!!");
-                break;
-               case 4:
-                System.out.println("Your Pasta is on the way ... ");
-                System.out.println("Thank you!!!");
-                break;
-        
-            default:
-                System.out.println("Invalid Choice");
-                break;
-        }
- */
-
-
-        int num;
-        System.out.println("Enter the no - ");
-        num = sc.nextInt();
-
-        System.out.println("Traffic Signal ");
-        System.out.println("1.Red ");
-        System.out.println("2.Orange ");
-        System.out.println("3.Green ");
-
-        switch (num) {
-            case 1:
-                System.out.println("Wait until the single turns green ");
-                break;
-            case 2:
-                System.out.println("Be ready single is about to turn green  ");
-                break;
-            case 3:
-                System.out.println("Go... ");
-                break;
-            
-            default:
-                System.out.println("INVALID INPUT !!! TRY AGAIN");
-                break;
-        }
-        
-
-
-
     }
+
 }
