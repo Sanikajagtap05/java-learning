87c95a4a22cd839cb07026fef277730c0bba9b01
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-22 18:22:38 +0530

    Code 10 file for while loops is started

---
+++ Code10.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Code10 {
+
+    // loops
+    // while loop pratice questions
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /*
+         * int i =1;
+         * while (i <=10){
+         * System.out.println(i);
+         * i++;
+         * }
+         * 
+         */
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * 
+         * int num;
+         * 
+         * System.out.println("Enter the no -");
+         * num = sc.nextInt();
+         * 
+         * System.out.println("You entered: " + num);
+         * 
+         * while (num <= 100){
+         * System.out.println(num);
+         * num++;
+         * 
+         * }
+         * 
+         */
+ 
+        /*
+        int num;
+        System.out.println("Enter the no -");
+        num = sc.nextInt();
+
+        while (num <= 20){
+            if (num % 2 == 1) {
+                System.out.println(num);
+            }
+            num++;
+        }
+  */
+    }
+
+}
