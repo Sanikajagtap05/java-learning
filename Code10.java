acf2848887fd6c46b7e289c39a73723abee0a18e
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-22 21:47:36 +0530

    practiced code for Sum of 1 to n

--- Code10.java
+++ Code10.java
@@ -32,19 +32,90 @@ public class Code10 {
          * }
          * 
          */
- 
-        /*
-        int num;
-        System.out.println("Enter the no -");
-        num = sc.nextInt();
 
-        while (num <= 20){
-            if (num % 2 == 1) {
-                System.out.println(num);
-            }
-            num++;
+        /*
+         * int num;
+         * System.out.println("Enter the no -");
+         * num = sc.nextInt();
+         * 
+         * while (num <= 20){
+         * if (num % 2 == 1) {
+         * System.out.println(num);
+         * }
+         * num++;
+         * }
+         */
+        /*
+         * int num;
+         * System.out.println("Enter the no -");
+         * num =sc.nextInt();
+         * 
+         * while(num <= 20){
+         * if(num % 2 == 0){
+         * System.out.println(num);
+         * 
+         * }
+         * num++;
+         * 
+         * }
+         */
+
+        /*
+         * int num ;
+         * System.out.println("Enter the no of which you require multiplication - ");
+         * num =sc.nextInt();
+         * int i= 1;
+         * while(i<= 10){
+         * System.out.println(num + "x" +i+ "=" +(num*i));
+         * i++;
+         * }
+         * 
+         */
+
+        /*
+         * int num;
+         * System.out.println("Enter the no  - ");
+         * num =sc.nextInt();
+         * int i = 1;
+         * 
+         * int sum =0;
+         * while( i<= num){
+         * sum = sum+i;
+         * i++;
+         * 
+         * }
+         * System.out.println("Sum = " +sum);
+         */
+
+        /*
+         * System.out.println("Enter the no  - ");
+         * int n = sc.nextInt();
+         * 
+         * int sum = 0;
+         * int i =1;
+         * while(i<=n){
+         * sum = sum +i;
+         * i++;
+         * 
+         * 
+         * }
+         * System.out.println("Sum = "+sum);
+         */
+ 
+        /* 
+        int sum = 0;
+        System.out.println("Enter the no - ");
+        int n= sc.nextInt();
+        int i = 1;
+        while(i<=n){
+            sum = sum +i;
+            i++;
         }
-  */
+        System.out.println("Sum = "+sum);
+
+        */
+
+        
     }
 
 }
