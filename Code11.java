241ba8aad2e455d38342c3c11bf7540969ea64f5
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-24 22:23:18 +0530

    practiced 3 more questions

--- Code11.java
+++ Code11.java
@@ -30,17 +30,70 @@ public class Code11 {
             i++;
         }while(i <= 20);
    */
-
+        /* 
          System.out.println("Enter the no of which mul");
          int n =sc.nextInt();
         int i = 1;
          do{
-            System.out.println( n "*" +)
-         }
+            System.out.println( n +"*" + i +" = "+(n*i));
+            i++;
+         }while(i<=10);
+        */
 
+         /* 
+         System.out.println("Enter the no until which which  sum is required -");
+         int n = sc.nextInt();
 
+        int i = 0;
+        int sum =0;
+        do{
+            sum = sum + i;
+            i++;
 
+        }while(i<=n);
+        System.out.println("sum ="+sum);
 
+        */
+       /* 
+        int sum =0; 
+        int n;
+        
+        do{
+            System.out.println("Enter the 0 to stop-");
+            n =sc.nextInt();
+            sum = sum + n;
+
+        }while(n !=0);
+
+        System.out.println("sum = "+sum);
+
+*/
+/* 
+  int n ;
+  int sum = 0;
+
+  do{
+    System.out.println( "Enter the no - ");
+    n =sc.nextInt();
+    sum = sum + n;
+
+  }while(n!=0);
+
+  System.out.println("Sum = "+sum);
+
+  */
+/* 
+  int pass;
+  do{
+    
+    System.out.println("Enter the password : ");
+    pass = sc.nextInt();
+  
+  }while(pass != 1234);
+  System.out.println("correct password");
+
+  */
+ 
     }
 
 }
