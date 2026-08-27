d79c05774aba658611c17e8f493702a024418af0
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-28 00:13:01 +0530

    Practice more 2-3 codes

--- Code13.java
+++ Code13.java
@@ -73,6 +73,67 @@ public class Code13 {
             System.out.println();
         }
         */
+       /* 
+       for(int i = 1; i <= 4; i++)
+{
+    for(int j = 1; j <= 2; j++)
+    {
+        System.out.print(i + " ");
+    }
+
+    System.out.println();
+}
+    */
+  /* 
+for(int i = 1; i <= 5; i++)
+{
+    for(int j = 1; j <= 4; j++)
+    {
+        System.out.print(" Hello ");
+    }
+    System.out.println();
+}
+    */
+ /* 
+for(int i = 1; i <= 3; i++)
+{
+    for(int j = 1; j <= i; j++)
+    {
+        System.out.print("* ");
+    }
+
+    System.out.println();
+}
+*/
+/* 
+for(int i = 1; i <= 2; i++)
+{
+    for(int j = 1; j <= 3; j++)
+    {
+        System.out.print(j + " ");
+    }
+    System.out.println();
+}
+    */
+   /* 
+   for(int i = 1; i <= 3; i++)
+{
+    for(int j = 1; j <= 2; j++)
+    {
+        System.out.print(i + " ");
+    }
+    System.out.println();
+}
+    */
+
+for(int i = 1; i <= 3; i++)
+{
+    for(int j = 1; j <= 3; j++)
+    {
+        System.out.print(i + "," + j + " ");
+    }
+    System.out.println();
+}
     }
 
 }
