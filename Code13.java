f454abb4c75b7d441e951442bfb831fa356367a3
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-27 17:36:39 +0530

    Practiced and learnt how upper loop and inner loop work

--- Code13.java
+++ Code13.java
@@ -9,6 +9,7 @@ public class Code13 {
         }
         */
 
+        /*
         for(int i = 1; i <= 3; i++)
 {
     for(int j = 1; j <= 3; j++)
@@ -18,6 +19,20 @@ public class Code13 {
 
     System.out.println();
 }
+*/
+/* 
+for(int i = 1; i <= 3; i++)// vertical row
+{
+    for(int j = 1; j <= 4; j++)//horizontal column
+    {
+        System.out.print("* ");
+    }
+
+    System.out.println();
+}
+*/
+
+
     }
 
 }
