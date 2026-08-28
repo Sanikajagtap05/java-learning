4494201ca94f49d214ec035e394e6251d2c28c36
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-28 23:53:33 +0530

    practiced more questions target is to practice atleast 50 questions

--- Code13.java
+++ Code13.java
@@ -170,6 +170,45 @@ for (int i=6;i>=1;i--){
 }
 
 */
+/* 
+ for(int i = 1;i<=5;i++){
+    for(int j = 1;j<=i;j++){
+        System.out.print(j+" ");
+    }
+    System.out.println();
+ }
+ */
+/* 
+ for(int i =5;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        System.out.print( i+ " ");
+    }
+    System.out.println();
+ }
+*/
+/* 
+for(int i = 5;i>=1;i--){
+    for(int j=5;j>=i;j--){
+        System.out.print(j+" ");
+    }
+    System.out.println();
+}
+*/
+/* 
+for(int i = 6;i>=1;i--){
+    for(int j = 6;j>=i;j--){
+        System.out.print( j +" ");
+    }
+    System.out.println();
+}
+    */
+
+for(int i = 5;i>=1;i--){
+    for(int j =i;j<=5;j++){
+        System.out.print(j + " ");
+    }
+    System.out.println();
+}
     }
 
 }
