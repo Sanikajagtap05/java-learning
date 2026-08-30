0fbfb8946685b09f8e2bc17510ccb714c417cb83
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-30 21:04:36 +0530

    practiced more patterns

--- Code13.java
+++ Code13.java
@@ -202,9 +202,38 @@ for(int i = 6;i>=1;i--){
     System.out.println();
 }
     */
+   /* 
 
 for(int i = 5;i>=1;i--){
     for(int j =i;j<=5;j++){
+        System.out.print(j + " ");
+    }
+    System.out.println();
+}
+
+*/
+/* 
+
+ for(int i = 1;i<=5;i++){
+   for(int j=i;j<=5;j++){
+        System.out.print(j+ " ");
+    }
+    System.out.println( );
+}
+
+*/
+
+/* 
+for(int i = 1;i<=5;i++){
+    for(int j = 5;j>=i;j--){
+        System.out.print(j+ " ");
+    }
+    System.out.println();
+}
+    */
+
+for(int i = 5 ;i>=1;i--){
+    for(int j =i;j>=1;j--){
         System.out.print(j + " ");
     }
     System.out.println();
