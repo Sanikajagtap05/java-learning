0ebee77c52af7be24aa795b3f4cc2e8f9e83d82a
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-08-30 21:32:46 +0530

    Started practico=ing patterns with including one more loop for spaces

--- Code13.java
+++ Code13.java
@@ -231,6 +231,7 @@ for(int i = 1;i<=5;i++){
     System.out.println();
 }
     */
+   /* 
 
 for(int i = 5 ;i>=1;i--){
     for(int j =i;j>=1;j--){
@@ -238,6 +239,29 @@ for(int i = 5 ;i>=1;i--){
     }
     System.out.println();
 }
+    */
+   /* 
+for(int i =1;i<=5;i++){
+    for(int j =1;j<=i;j++){
+        if(j % 2 == 1){
+            System.out.print("1");
+        }else{
+            System.out.print(" 0 ");
+        }
+
+    }
+    System.out.println();
+}
+    */
+ for (int i= 5;i>=1;i--){
+    for(int k=5; k>i;k-- ){
+        System.out.print( " ");
+    }
+    for(int j =1;j<=i;j++){
+        System.out.print(j);
+    }
+    System.out.println( );
+ }
     }
 
 }
