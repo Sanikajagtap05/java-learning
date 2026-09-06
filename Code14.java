a96e08535f85a74c9881f5d029bd3ac098ba14a0
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-09-06 22:04:58 +0530

    Practiced  array and started array practiced array

---
+++ Code14.java
@@ -0,0 +1,67 @@
+public class Code14 {
+    /**
+     * @param args
+     */
+    public static void main(String args[]){
+        /* 
+        int marks[] = {20,75,60,50,90};
+
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+       */
+
+        /* 
+        int marks[] = {20,75,60,50,90};
+        System.out.println("length of the array is : " + marks.length);
+        */
+
+        /* 
+        int marks[] = {20,75,60,50,90};
+        System.out.println(marks[4]);
+        System.out.println(marks[3]);
+        System.out.println(marks[2]);
+        System.out.println(marks[1]);
+        System.out.println(marks[0]);
+        */
+
+        /* 
+        int marks[] = {20,75,60,50,90};
+        for(int i=0;i<marks.length;i++){
+            System.out.println("Index "+ i + "=" +marks[i]);
+        }
+        */
+       /* 
+        //print only value at even index
+        int marks[] = {20,75,60,50,90};
+        for(int i=0;i<marks.length;i++){
+            if(i%2 ==0)
+        {
+            System.out.println( "Index "+ i + "=" +marks[i]);
+        }
+        */
+        /* 
+        int marks[]={908,70,87,50,76};
+
+        for(int i=0;i<marks.length;i++){
+            if(marks.length%2==1)
+                {
+                System.out.println("Index "+ i + "=" +marks[i]);
+            }
+        }
+ 
+        }
+
+        */
+       
+       //print reverse order of array
+       int marks[]={908,70,87,50,76};
+
+        for(int i=0;i<marks.length;i++){
+            if(marks.length%2==){
+
+            }
+            
+        }
+    }
+}
