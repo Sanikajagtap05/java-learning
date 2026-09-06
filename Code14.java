8a541a67ca2e654f011d244858921930c5cef6af
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-09-06 22:50:40 +0530

    practiced more codes and tried  thinking  questions

--- Code14.java
+++ Code14.java
@@ -2,66 +2,99 @@ public class Code14 {
     /**
      * @param args
      */
-    public static void main(String args[]){
-        /* 
-        int marks[] = {20,75,60,50,90};
+    public static void main(String args[]) {
+        /*
+         * int marks[] = {20,75,60,50,90};
+         * 
+         * for(int i=0;i<marks.length;i++){
+         * System.out.println(marks[i]);
+         * }
+         */
 
-        for(int i=0;i<marks.length;i++){
-            System.out.println(marks[i]);
+        /*
+         * int marks[] = {20,75,60,50,90};
+         * System.out.println("length of the array is : " + marks.length);
+         */
+
+        /*
+         * int marks[] = {20,75,60,50,90};
+         * System.out.println(marks[4]);
+         * System.out.println(marks[3]);
+         * System.out.println(marks[2]);
+         * System.out.println(marks[1]);
+         * System.out.println(marks[0]);
+         */
+
+        /*
+         * int marks[] = {20,75,60,50,90};
+         * for(int i=0;i<marks.length;i++){
+         * System.out.println("Index "+ i + "=" +marks[i]);
+         * }
+         */
+        /*
+         * //print only value at even index
+         * int marks[] = {20,75,60,50,90};
+         * for(int i=0;i<marks.length;i++){
+         * if(i%2 ==0)
+         * {
+         * System.out.println( "Index "+ i + "=" +marks[i]);
+         * }
+         */
+        /*
+         * int marks[]={908,70,87,50,76};
+         * 
+         * for(int i=0;i<marks.length;i++){
+         * if(marks.length%2==1)
+         * {
+         * System.out.println("Index "+ i + "=" +marks[i]);
+         * }
+         * }
+         * 
+         * }
+         * 
+         */
+        /*
+         * //print reverse order of array
+         * int marks[]={908,70,87,50,76};
+         * 
+         * for(int i=marks.length-1;i>=0;i--){
+         * System.out.println("Index "+ i + "=" +marks[i]);
+         * }
+         * 
+         */
+
+        /*
+         * //Again
+         * int marks[]={98,70,87,50,76};
+         * for(int i=marks.length-1;i>=0;i--){
+         * System.out.println("Index" +i+ "=" +marks[i]);
+         * }
+         */
+
+        // Thinking Questions
+        /*
+         * int a[] = {1,2,3,4,5};
+         * for(int i =0;i<a.length;i++){
+         * System.out.println(a[i]);
+         * }//final value of i is 5
+         */
+        /*  
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i <= numbers.length; i++) {
+            System.out.println(numbers[i]);
         }
-       */
 
-        /* 
-        int marks[] = {20,75,60,50,90};
-        System.out.println("length of the array is : " + marks.length);
+        //in this case in for loop i<=numbers.length  is wrong there should  be i<numbers.length because array index starts from 0 to n-1
         */
+       /*
+        // print the elements 82,91,65  without using loop
+         int marks[] = { 82, 91, 55, 70, 55,65  };
+          System.out.println(marks[0]);
+          System.out.println(marks[1]);
+          System.out.println(marks[5]);
+ */ 
+ //Chnage the Element of array
 
-        /* 
-        int marks[] = {20,75,60,50,90};
-        System.out.println(marks[4]);
-        System.out.println(marks[3]);
-        System.out.println(marks[2]);
-        System.out.println(marks[1]);
-        System.out.println(marks[0]);
-        */
-
-        /* 
-        int marks[] = {20,75,60,50,90};
-        for(int i=0;i<marks.length;i++){
-            System.out.println("Index "+ i + "=" +marks[i]);
-        }
-        */
-       /* 
-        //print only value at even index
-        int marks[] = {20,75,60,50,90};
-        for(int i=0;i<marks.length;i++){
-            if(i%2 ==0)
-        {
-            System.out.println( "Index "+ i + "=" +marks[i]);
-        }
-        */
-        /* 
-        int marks[]={908,70,87,50,76};
-
-        for(int i=0;i<marks.length;i++){
-            if(marks.length%2==1)
-                {
-                System.out.println("Index "+ i + "=" +marks[i]);
-            }
-        }
- 
-        }
-
-        */
-       
-       //print reverse order of array
-       int marks[]={908,70,87,50,76};
-
-        for(int i=0;i<marks.length;i++){
-            if(marks.length%2==){
-
-            }
-            
-        }
     }
 }
